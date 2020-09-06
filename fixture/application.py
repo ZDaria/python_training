9ad37072b80c83329b2a40768952a301e45466d6
ZDaria <zavgorodneva.daria@gmail.com>
2020-09-06 21:28:07 +0300

    Selected class for add contact tests

--- fixture/application.py
+++ fixture/application.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
+
 
 class Application:
     def __init__(self):
@@ -9,6 +10,7 @@ class Application:
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
@@ -16,100 +18,3 @@ class Application:
 
     def destroy(self):
         self.wd.quit()
-
-    def create_new_contact(self, contact):
-        wd = self.wd
-        self.open_new_contact_page()
-        # name info
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        # work info
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("theform").click()
-        # telephone info
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.telephone_home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.telephone_mobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.telephone_work)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        # internet info
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        # birthday
-        wd.find_element_by_name("bday").click()
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        wd.find_element_by_xpath("//option[@value='23']").click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element_by_xpath("//option[@value='March']").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aday)
-        wd.find_element_by_xpath("(//option[@value='1'])[2]").click()
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
-        wd.find_element_by_xpath("(//option[@value='September'])[2]").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contact.ayear)
-        wd.find_element_by_name("new_group").click()
-        wd.find_element_by_xpath("//option[@value='[none]']").click()
-        # additional info
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        wd.find_element_by_xpath("//div[@id='content']/div").click()
-        self.return_to_home_page()
-
-    def return_to_home_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home").click()
-
-    def open_new_contact_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,27 +13,25 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_new_contact(Contact(firstname="Ivan",
-                                   middlename="Ivanovich",
-                                   lastname="Ivanov",
-                                   nickname="Vany123",
-                                   title="QA",
-                                   company="Alfa Bank",
-                                   address="Russion Federation\nMoscow\nRead sq 1",
-                                   telephone_home="84955123545",
-                                   telephone_mobile="89105121211",
-                                   telephone_work="84957121911",
-                                   email="test@gmail.com",
-                                   homepage="-",
-                                   bday="23",
-                                   bmonth="March", byear="1990",
-                                   aday="1", amonth="September", ayear="2021",
-                                   address2="-",
-                                   phone2="-",
-                                   notes="-",
-                                   fax="-",
-                                   email2="-",
-                                   email3="-"))
-
-
+    app.contact.create(Contact(firstname="Ivan",
+                               middlename="Ivanovich",
+                               lastname="Ivanov",
+                               nickname="Vany123",
+                               title="QA",
+                               company="Alfa Bank",
+                               address="Russion Federation\nMoscow\nRead sq 1",
+                               telephone_home="84955123545",
+                               telephone_mobile="89105121211",
+                               telephone_work="84957121911",
+                               email="test@gmail.com",
+                               homepage="-",
+                               bday="23",
+                               bmonth="March", byear="1990",
+                               aday="1", amonth="September", ayear="2021",
+                               address2="-",
+                               phone2="-",
+                               notes="-",
+                               fax="-",
+                               email2="-",
+                               email3="-"))
     app.session.logout()
